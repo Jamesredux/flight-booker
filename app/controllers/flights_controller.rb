588d035cd21b3bb0fc5e2f8df29a817bbdf64402
James McCann <jamesredux@gmail.com>
2018-12-31 08:24:12 +0700

    config time zone

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,14 @@ class FlightsController < ApplicationController
   def index
   	@airports = Airport.all.map{ |airport| [ airport.name, airport.id ] }
   	@passenger_options = [1,2,3,4].map{ |x| [x,x] }
-
   	@flight_days = Flight.list_dates
+  	@flights = Flight.search(params[:from_airport], params[:to_airport], params[:chosen_date])
+  
   end
+
+
+
+  private
+
+ 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,7 +6,7 @@ class Flight < ApplicationRecord
 	validates :to_airport_id, presence: true
 	validates :dep_time, presence:true
 	validates :duration, presence: true
-
+	
 	def self.list_dates
 		dates = Flight.all.order(:dep_time)
 		dates = dates.map { |d| [d.dep_time.strftime("%d %^b %Y"), d.dep_time.strftime("%Y%m%d") ] }
@@ -14,6 +14,23 @@ class Flight < ApplicationRecord
 	
 		
 	end
+
+	def self.search(from, destination, date)
+			Flight.where(from_airport_id:  from,
+  									to_airport_id: destination)
+  									#dep_time: Flight.within_date(date))
+	end
+
+
+	def self.within_date(date)
+		unless date.nil?
+			date = date.to_date
+			date.beginning_of_day..date.end_of_day
+		end	
+		
+	end
+
+	private
 
 
 
