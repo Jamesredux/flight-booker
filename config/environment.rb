0a62aeca47ab34cfb5ffed307134eef443277d8a
James McCann <jamesredux@gmail.com>
2019-07-30 19:49:10 +0700

    fix error in sendgrid settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.stmp_settings = {
+ActionMailer::Base.smtp_settings = {
 	:user_name => ENV['SENDGRID_USERNAME'],
 	:password => ENV['SENDGRID_PASSWORD'],
 	:domain => 'https://redux-flights.herokuapp.com/',
-	:address => 'stmp.sendgrid.net',
+	:address => 'smtp.sendgrid.net',
 	:port => 587,
 	:authentication => :plain,
 	:enable_starttls_auto => true
