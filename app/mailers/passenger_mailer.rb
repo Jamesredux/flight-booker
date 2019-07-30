3de3e59e81fa0bf815dc89a8844b3fa244d31000
James McCann <jamesredux@gmail.com>
2019-07-30 18:51:49 +0700

    development mailer added

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,13 +2,12 @@ class PassengerMailer < ApplicationMailer
 	default from: 'noreply@flightbooker.com'
 
 	def thankyou_mail
-#change to take an argument from booking.create
 		@booking = params[:booking]
 		@passengers = @booking.passenger_ids
 		@booker = Passenger.find(@passengers.first)
 		@booking_ref = @booking.id
 		
 		@url = "http://localhost:3000/bookings/search"
-		mail(to: @booker.email, Subject: 'Thankyou for booking your flight')
+		mail(to: @booker.email, Subject: 'Thank you, your Flight has been booked!')
 	end
 end
